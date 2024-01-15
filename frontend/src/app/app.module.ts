ef159651f6294b2f0d92c1ff2fb84833ac315fb6
PacoSMTD <francisco.sanchez@tdconsulting.es>
2024-01-15 13:15:01 +0100

    comit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ValidadorComponent } from './pages/validador/validador.component';
+import { ElementoSimpleComponent } from './componentes/elemento-simple/elemento-simple.component';
+import { ElementoDobleComponent } from './componentes/elemento-doble/elemento-doble.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValidadorComponent,
+    ElementoSimpleComponent,
+    ElementoDobleComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/pages/validador/validador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validador',
+  templateUrl: './validador.component.html',
+  styleUrls: ['./validador.component.css']
+})
+export class ValidadorComponent {
+
+}
