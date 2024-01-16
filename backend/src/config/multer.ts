0014c1d3b905a1edf8c2478ceb1a635ddb708e5d
Christian <christian.berdejo@tdconsulting.es>
2024-01-16 10:57:22 +0100

    back config started

---
+++ backend/src/config/multer.ts
@@ -0,0 +1,27 @@
+import multer from 'multer';
+import path from 'path';
+
+// Función para crear la configuración de almacenamiento
+const createStorageConfig = (folderPath: string) => {
+  return multer.diskStorage({
+    destination: path.join(__dirname, '..', '..', 'assets', 'images', folderPath),
+    filename: (_req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const fileName = `${uniqueSuffix}${path.extname(file.originalname)}`;
+      cb(null, fileName);
+    },
+  });
+};
+
+// Crear configuraciones de almacenamiento específicas
+const storageImagenRepresentativa = createStorageConfig('imagen_representativa');
+const storageImagenProcesada = createStorageConfig('imagen_procesamiento');
+
+// Middleware de Multer
+
+
+const uploadImagenRepresentativa = multer({ storage: storageImagenRepresentativa }).array('movilImage');
+const uploadImagenProcesada = multer({ storage: storageImagenProcesada }).array('movilImage');
+
+// Exportar los middleware
+export { uploadImagenRepresentativa, uploadImagenProcesada };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+
 
 const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
