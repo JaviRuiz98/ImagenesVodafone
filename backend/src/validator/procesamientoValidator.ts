46451a8cb98e9ead1bfa4ea4d989409feb18aae8
raulperezjorda-td <perezjordaraul@gmail.com>
2024-01-18 13:09:25 +0100

    nuevos mensajes error castellano

--- backend/src/validator/procesamientoValidator.ts
+++ backend/src/validator/procesamientoValidator.ts
@@ -8,11 +8,11 @@ export async function procesamientoValidator(req: Request, res: Response, next: 
     const idExpositorio: number = req.body.idExpositorio;
 
     if (!files['imagenesProcesamiento'] || files['imagenesProcesamiento'].length === 0) {
-        res.status(400).json({ error: 'An array of images is required' });
+        res.status(400).json({ error: 'Es necesario un array de imagenes' });
     }
 
     if (!idExpositorio) {
-        res.status(400).json({ error: 'Expositorio is required' });
+        res.status(400).json({ error: 'Expositor es necesario' });
     }
 
     next();
