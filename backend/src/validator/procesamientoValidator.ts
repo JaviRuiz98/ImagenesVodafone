0be9c0e585eff6638a00006ea9240a2a79d412bc
Christian <christian.berdejo@tdconsulting.es>
2024-01-18 13:19:21 +0100

    file fix?

--- backend/src/validator/procesamientoValidator.ts
+++ backend/src/validator/procesamientoValidator.ts
@@ -4,11 +4,11 @@ import { Request, Response, NextFunction } from 'express';
 
 export async function procesamientoValidator(req: Request, res: Response, next: NextFunction) {
 
-    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    const file = req.file;
     const idExpositorio: number = req.body.idExpositorio;
 
-    if (!files['imagenesProcesamiento'] || files['imagenesProcesamiento'].length === 0) {
-        res.status(400).json({ error: 'Es necesario un array de imagenes' });
+    if (!file) {
+        res.status(400).json({ error: 'Es necesario una imagen' });
     }
 
     if (!idExpositorio) {
