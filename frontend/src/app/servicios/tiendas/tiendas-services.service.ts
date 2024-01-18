8e4e4f7d89ee3c7d120a1072f88e354bffd9a5c9
JaviRuiz98 <115167166+JaviRuiz98@users.noreply.github.com>
2024-01-18 10:10:59 +0100

    Actualización Selector Imagenes

---
+++ frontend/src/app/servicios/tiendas/tiendas-services.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+import { tienda } from '../../interfaces/tienda';  
+import { mueble } from '../../interfaces/muebles';
+import { expositorios } from '../../interfaces/expositorios';
+import { imagenes } from '../../interfaces/imagenes';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+
+
+
+
+
+export class TiendasServicesService {
+
+  
+  tienda: tienda = {
+    id_tienda: 0,
+    sfid: 0,
+    muebles: []
+  }
+
+
+  API_URI = 'http://localhost:3000';
+
+
+  constructor(private http: HttpClient){ }
+
+  getTienda(sfid: number): Observable<tienda> {
+
+    const params =  { sfid: sfid };
+    return this.http.get<tienda>(`${this.API_URI}/tienda`, { params: params });
+
+  }
+
+  getMuebles(id_tienda: number): Observable<mueble[]> {
+
+    const params =  { id_tienda: id_tienda };
+    return this.http.get<mueble[]>(`${this.API_URI}/muebles`);
+  }
+ 
+  getExpositorios(id_mueble: number): Observable<expositorios[]> {
+    const params =  { id_mueble: id_mueble };
+    return this.http.get<expositorios[]>(`${this.API_URI}/expositorios`, { params: params });
+  }
+
+  getImagene(id_expositorio: number): Observable<imagenes[]> {
+    const params =  { id_expositorio: id_expositorio };
+    return this.http.get<imagenes[]>(`${this.API_URI}/imagenes`, { params: params });
+  }
+
+  getUrl(id_imagen: number): Observable<string> {
+    const params =  { id_imagen: id_imagen };
+    return this.http.get<string>(`${this.API_URI}/url`, { params: params });
+  }
+
+  
+
+
+
+
+
+}
