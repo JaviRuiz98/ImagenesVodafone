7a1f5d7d1250d1db45a6152ff7e32133d206f4b4
JaviRuiz98 <115167166+JaviRuiz98@users.noreply.github.com>
2024-01-18 12:27:32 +0100

    Delete frontend directory

--- frontend/src/app/interfaces/expositorios.ts
+++
@@ -1,9 +0,0 @@
-
-import { imagenes } from "./imagenes";
-import { dispositivos } from "./dispositivos";
-export interface expositorios {
-    id_expositorio: number;
-    id_mueble: number;
-    imagen: imagenes;
-    dispositivos: dispositivos[];
-}
--- frontend/src/app/interfaces/respuesta_movil.ts
+++
@@ -1,6 +0,0 @@
-export interface respuesta_movil {
-    id_respuesta_movil: number;
-    id_procesado_imagen: number;
-    huecos_contados: number;
-    moviles_contados: string;
-}
