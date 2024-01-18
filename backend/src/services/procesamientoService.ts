f60aa7e9050ac8c96a2be11d853a3889baa81dba
Christian <christian.berdejo@tdconsulting.es>
2024-01-18 13:59:03 +0100

    planteamiento guardado bbdd

---
+++ backend/src/services/procesamientoService.ts
@@ -0,0 +1,21 @@
+//import db  from "../config/database";
+
+export const procesamientoService = {
+    /*async create (id_imagen: number, id_expositor: number, comentarios: string, valido: boolean, IA_utilizada: string ) {
+
+        const procesamiento = await db.procesados_imagenes.create({
+            data: {
+                id_imagen: id_imagen,
+                id_expositor: id_expositor,
+                comentarios: comentarios,
+                valido: valido,
+                IA_utilizada: IA_utilizada
+            }
+        });
+
+        //falta crear respuesta en la base de datos con el procesamiento hecho y devolverlo
+    }, */
+
+
+
+}
