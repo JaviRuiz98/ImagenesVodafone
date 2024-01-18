d315901357c8358e5220d9d0062020b5dffaf714
raulperezjorda-td <perezjordaraul@gmail.com>
2024-01-18 13:09:25 +0100

    nuevos mensajes error castellano

--- backend/src/validator/tiendaValidator.ts
+++ backend/src/validator/tiendaValidator.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from 'express';
 export async function validateGetTiendasBySfid(req:Request, res:Response, next:NextFunction) {
     const sfid = req.params.sfid;
     if (!sfid) {
-        res.status(400).json({ error: 'Sfid is required' });
+        res.status(400).json({ error: 'Sfid es necesario' });
     }
     next();
 }
