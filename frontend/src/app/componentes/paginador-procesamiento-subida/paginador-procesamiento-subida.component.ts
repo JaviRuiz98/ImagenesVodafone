addea1a026f9ba56e6fba97865211cacd2dfcfe5
PacoSMTD <francisco.sanchez@tdconsulting.es>
2024-01-23 11:51:19 +0100

    s

--- frontend/src/app/componentes/paginador-procesamiento-subida/paginador-procesamiento-subida.component.ts
+++ frontend/src/app/componentes/paginador-procesamiento-subida/paginador-procesamiento-subida.component.ts
@@ -5,7 +5,7 @@ import { procesados_imagenes } from 'src/app/interfaces/procesados_imagenes';
 import { SelectorImagenesComponent } from '../selector-imagenes/selector-imagenes.component';
 import { TagModule } from 'primeng/tag';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-
+ 
 @Component({
     selector: 'app-paginador-procesamiento-subida',
     templateUrl: './paginador-procesamiento-subida.html',
@@ -25,12 +25,15 @@ export class PaginadorProcesamientoSubidaComponent {
     @Input() procesados: procesados_imagenes[] = [];
     @Input() cargando_procesamiento: boolean = false;
     @Input() id_expositor_selected: number = 0;
+    @Output() dialog_procesado: procesados_imagenes[] = [];
+    @Output() mostrar_info: boolean = false;
     @Output() archivoSeleccionadoChange = new EventEmitter<{ archivo: File, id_expositor_selected: number }>();
 
     url_imagenes_procesadas: string = 'http://localhost:3000/imagenesProcesamiento/';
 
     items_per_page: number = 1;
     indice_paginador: number = 0;
+    visible_info_cartel: boolean = false;
 
     onPageChange(event: any) {
         this.indice_paginador = event.first;
@@ -80,4 +83,11 @@ export class PaginadorProcesamientoSubidaComponent {
             return 'warning';
         }
     }
+
+    onMouseOverCartel(event: MouseEvent) {
+        this.visible_info_cartel = !this.visible_info_cartel;
+    }
+
+
+
 }
