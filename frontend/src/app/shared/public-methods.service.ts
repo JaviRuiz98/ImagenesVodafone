1d96accc5bd6d1a88be1555caf918bff75f5f7ad
raulperezjorda-td <raul.perez@tdconsulting.es>
2024-01-25 12:16:56 +0100

    switch actualizado

--- frontend/src/app/shared/public-methods.service.ts
+++ frontend/src/app/shared/public-methods.service.ts
@@ -13,19 +13,17 @@ export class PublicMethodsService {
             return 'success' as string;
  
         case 'alta':
-            return 'warning' as string; 
         case 'media':
+        case 'otro idioma':
             return 'warning' as string;
 
         case 'baja':
-            return 'danger' as string;
         case 'muy baja':
-            return 'danger' as string;
         case 'ninguna':
             return 'danger' as string;
        
         default:
-            return '';
+            return 'primary';
     }
   };
 
