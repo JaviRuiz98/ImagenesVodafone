1397dc44a5e0764d99c260a91b92853a30f408ba
PacoSMTD <francisco.sanchez@tdconsulting.es>
2024-01-26 09:47:19 +0100

    e

--- frontend/src/app/componentes/paginador-procesamiento-subida/paginador-procesamiento-subida.component.ts
+++ frontend/src/app/componentes/paginador-procesamiento-subida/paginador-procesamiento-subida.component.ts
@@ -88,25 +88,4 @@ export class PaginadorProcesamientoSubidaComponent {
         this.cargando_procesamiento = true;
     }
 
-    onMouseOver(event: MouseEvent) {
-        this.visible_info_procesamiento = true;
-        const tagElement = event.target as HTMLElement;
-     //   tagElement.classList.add('cursor-zoom');
-    }
-
-    onMouseOut(event: MouseEvent) {
-        this.visible_info_procesamiento = false;
-    }
-
-    onMouseClick(event: any) {
-        this.visible_info_procesamiento_click = !this.visible_info_procesamiento_click;
-    }
- 
-    getSeverityCartel(procesado: string): string {
-        return this.publicMethodsService.getSeverityCartel(procesado);
-    }
-    
-    getSeverityDispositivos(numero_dispositivos: number, huecos_esperados: number): string {
-    return this.publicMethodsService.getSeverityDispositivos(numero_dispositivos, huecos_esperados);
-    }
 }
