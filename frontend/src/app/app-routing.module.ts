c5937af6e02122c26d481b3f0cae0475184c8528
raulperezjorda-td <raul.perez@tdconsulting.es>
2024-02-01 13:47:39 +0100

    home es validador

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
 
-  { path: "", component: HomeComponent },
+  { path: "", component: ValidadorComponent },
 
 ];
 
