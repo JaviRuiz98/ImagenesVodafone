54f612edadd637f8cbb5e89e41ea78ad0bdfb967
javiruiz1998 <fjrh98@gmail.com>
2024-02-01 11:41:00 +0100

    home creado

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'primeng/dropdown';
 import { HomeComponent } from './home.component';
-
-
-
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'primeng/api';
+import { BarraMenuComponent } from 'src/app/componentes/barra-menu/barra-menu.component';
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
     CommonModule,
-    DropdownModule
+    DropdownModule,
+    FormsModule,
+    SharedModule,
+    BarraMenuComponent
   ]
 })
 export class HomeModule { }
