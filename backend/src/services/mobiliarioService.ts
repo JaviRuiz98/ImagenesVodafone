f199972fe4ecc3fb96861cc2eaa2f5d647dd7801
Christian <christian.berdejo@tdconsulting.es>
2024-02-07 09:47:09 +0100

    fesc

--- backend/src/services/mobiliarioService.ts
+++ backend/src/services/mobiliarioService.ts
@@ -49,7 +49,7 @@ export const mobiliarioService = {
                                         
                                     },
                                 orderBy: {
-                                    
+                                    fecha: 'desc',
                                 },
         
                                 where: {
