6f27fef4c06642ec16638c9d51ccc1151559e5ed
Christian <christian.berdejo@tdconsulting.es>
2024-02-12 10:34:13 +0100

    mergeado

--- frontend/src/app/servicios/expositores/expositores.service.ts
+++ frontend/src/app/servicios/expositores/expositores.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Expositor } from '../../interfaces/expositor';
-import { imagenes } from '../../interfaces/imagenes';
+
 
 @Injectable({
   providedIn: 'root'
