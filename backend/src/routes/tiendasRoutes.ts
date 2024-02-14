88bf48877bacda53f5e4f313da4cd36aedafa5c5
javiruiz1998 <fjrh98@gmail.com>
2024-02-14 11:23:00 +0100

    alinear

--- backend/src/routes/tiendasRoutes.ts
+++ backend/src/routes/tiendasRoutes.ts
@@ -1,8 +1,6 @@
 
 import express from 'express';
 import { getAllTiendas, newTienda, updateTienda, activarDesactivarTienda} from '../controller/tiendasController';
-//import { validateGetTiendasBySfid } from '../validator/tiendaValidator';
-
 
 const router = express.Router();
 
@@ -10,10 +8,5 @@ router.get('/tiendas',getAllTiendas);
 router.post('/tiendas', newTienda);
 router.post('/tiendas/:id_tienda', updateTienda);
 router.put('/tiendas/', activarDesactivarTienda);
-
-
-
-//router.post('/tiendas/:sfid', validateGetTiendasBySfid, getTiendaBySfid);
-//router.post('/procesadosImagenes/:idExpositor', validateGetProcesadosByIdExpositor, getProcesadosByIdExpositor);
 
 export default router;
