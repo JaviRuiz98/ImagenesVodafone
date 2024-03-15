0d5b5217d8ce3d1ea16a074a544bc276d3c8731d
raulperezjorda-td <raul.perez@tdconsulting.es>
2024-03-15 09:13:49 +0100

    descarga FTP

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -16,7 +16,7 @@ const createStorageConfig = (folderPath: string) => {
 };
 
 function getDestination(folderPath: string): string {
- return  path.join(__dirname, '..', '..', 'assets', 'imagenes', folderPath);
+  return 'http://validador-vf.topdigital.local/imagenes/'+ folderPath + '/'
 }
 // Crear configuraciones de almacenamiento específicas
 const storageImagenReferencia = createStorageConfig('imagenesReferencia');
