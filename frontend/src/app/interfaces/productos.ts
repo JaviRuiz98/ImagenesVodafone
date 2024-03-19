9a381218e11b0bb9116b74d7b200da41415633b4
raulperezjorda-td <raul.perez@tdconsulting.es>
2024-03-19 13:35:33 +0100

    soluciones merge dev defectuoso

--- frontend/src/app/interfaces/productos.ts
+++ frontend/src/app/interfaces/productos.ts
@@ -9,7 +9,9 @@ export class productos{
     descripcion: string;
     imagenes: imagenes;
     caracteristicas_productos: caracteristicas_productos[]
+
     caracteristica_seleccionada?: caracteristicas_productos;
+    cantidad?: number;
 
     constructor(id: number, nombre: string, precio: number, descripcion: string, imagenes: imagenes, caracteristicas_productos: caracteristicas_productos[]){
         this.id = id;
--- frontend/src/app/pages/uniformes/uniformes.component.ts
+++ frontend/src/app/pages/uniformes/uniformes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UniformesModule } from './uniformes.module';
 import { productos } from 'src/app/interfaces/productos';
 import { UniformesService } from 'src/app/servicios/uniformes/uniformes.service';
 import { CarritoComponent } from 'src/app/componentes/carrito/carrito.component';
@@ -18,6 +17,8 @@ import { caracteristicas_productos } from 'src/app/interfaces/caracteristicas';
 
 export class UniformesComponent implements OnInit {
 
+  constructor( private uniformesService: UniformesService ) { }
+
   productos!: productos[];
 
   caracteristicas_productos!: caracteristicas_productos[];
@@ -26,17 +27,6 @@ export class UniformesComponent implements OnInit {
   verOpcionesProducto: boolean = false;
   opciones_producto!: productos[];
   url_imagenes_productos: string = 'http://validador-vf.topdigital.local/imagenes/imagenesProducto/';
-
-  constructor(private formBuilder: FormBuilder, private uniformesService: UniformesService,private fb: FormBuilder ) {
-    // this.formulario = this.fb.group({
-    //   mueble: this.fb.group({
-    //     id: [],
-    //     nombre_mueble: ['', Validators.required],
-    //     region: [''],
-    //     expositores: this.fb.array([]) // Ahora es un FormArray
-    //   }),
-    // });
-  }
 
 
   nuevoProducto(){
@@ -53,58 +43,32 @@ export class UniformesComponent implements OnInit {
 
  
   anadirCarrito(producto: productos){
-    this.productos.find(productoSelect => {
-      if(productoSelect.id === producto.id){
-        producto.visible = !producto.visible;
-      }
-    })
-  }
 
-  deseleccionarOtrasTallas(opcion_caracteristica: Opciones_caracteristicas) {
-    const index = this.productos.findIndex(productito => productito.id === opcion_caracteristica.id_producto);
-
-    this.productos[index].opciones_caracteristicas.forEach(item => {
-      if (item.id !== opcion_caracteristica.id) {
-          item.seleccionado = false;
-      }
-      else{
-   //  item.seleccionado = item.seleccionado? false : true;
-        if(item.seleccionado){
-          item.seleccionado = false;
-        }else{
-          item.seleccionado = true;
-        } 
-      }
-    });
-    console.log(this.productos);
   }
 
   ngOnInit() {
     this.inicializaProductos();
-    this.formGroup = new FormGroup({
-      selectedCategory: new FormControl()
-  });
   }
 
   inicializaProductos() {
-    this.uniformesService.getProductos().subscribe((productos: productos[]) => {
-      this.productos = productos;
-      this.inicializaCaracteristicasProducto();
-    });
+    this.uniformesService.getProductos().subscribe(
+      (productos: productos[]) => {
+        this.productos = productos;
+        this.inicializaCaracteristicasProducto();
+      }
+    );
 
   }
 
   inicializaCaracteristicasProducto() {
-    this.uniformesService.getCaracteristicas().subscribe((caracteristicas: caracteristicas_productos[]) => {
-      this.caracteristicas_productos = caracteristicas;
-      this.productos.map(producto => {
-        producto.caracteristicas_productos = this.caracteristicas_productos.filter(caracteristica => caracteristica.id_producto === producto.id);
-
-      })
-      console.log(this.productos);
-    });
-    
+    this.uniformesService.getCaracteristicas().subscribe(
+      (caracteristicas: caracteristicas_productos[]) => {
+        this.caracteristicas_productos = caracteristicas;
+        this.productos.map(producto => {
+          producto.caracteristicas_productos = this.caracteristicas_productos.filter(caracteristica => caracteristica.id_producto === producto.id);
+        })
+        console.log(this.productos);
+      }
+    );
   }
-
-
 }
