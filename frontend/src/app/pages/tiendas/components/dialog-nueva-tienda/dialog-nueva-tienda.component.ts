81db28608f2eb416b2d22425302511cfe28f35ed
Christian <christian.berdejo@tdconsulting.es>
2024-03-19 13:03:36 +0100

    tiendahtml

--- frontend/src/app/pages/tiendas/components/dialog-nueva-tienda/dialog-nueva-tienda.component.ts
+++ frontend/src/app/pages/tiendas/components/dialog-nueva-tienda/dialog-nueva-tienda.component.ts
@@ -5,6 +5,7 @@ import { MueblesService } from 'src/app/servicios/muebles/muebles.service';
 
 import { tienda, tiendaCreacion } from 'src/app/interfaces/tienda';
 import { muebles } from 'src/app/interfaces/muebles';
+
 @Component({
   selector: 'app-dialog-nueva-tienda',
   templateUrl: './dialog-nueva-tienda.component.html',
@@ -23,6 +24,7 @@ export class DialogNuevaTiendaComponent{
 
   parametrosSteps: any; 
   activeIndex: number = 0;
+  maxStep: number = 5;
   contenidoBotonCrearEditarTienda: string = 'Siguiente';
   editarTiendaCreada: boolean = false;
   crearTienda: boolean = false;
@@ -81,15 +83,21 @@ export class DialogNuevaTiendaComponent{
         }
       },
       {
-        label: 'Confirmar',
+        label: 'Plano Tienda',
         command: (event: any) => {
           this.activeIndex = 4;
+        }
+      },
+      {
+        label: 'Confirmar',
+        command: (event: any) => {
+          this.activeIndex = this.maxStep;
         }
       }
     ];
   }
   botonSiguiente(){
-    if(this.activeIndex < 4){
+    if(this.activeIndex < this.maxStep){
       this.activeIndex++;
     } else{
       if(this.contenidoBotonCrearEditarTienda == 'Crear Tienda'){
