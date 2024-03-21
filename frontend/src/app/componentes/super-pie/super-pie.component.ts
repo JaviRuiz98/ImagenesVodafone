2c4fadc01bb0c3410bb0898bf53ce24593fed57b
raulperezjorda-td <raul.perez@tdconsulting.es>
2024-03-21 13:51:53 +0100

    interfaz anadida

--- frontend/src/app/componentes/super-pie/super-pie.component.ts
+++ frontend/src/app/componentes/super-pie/super-pie.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ChartModule } from 'primeng/chart';
+import { datos_graficas } from 'src/app/interfaces/datos_graficas';
 
 @Component({
   selector: 'app-super-pie',
@@ -12,4 +13,38 @@ import { ChartModule } from 'primeng/chart';
 })
 export class SuperPieComponent {
 
+  @Input() datos_graficas: datos_graficas[] = [];
+
+  data: any;
+
+  options: any;
+
+  ngOnInit() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+
+      this.data = {
+          labels: this.datos_graficas.map((d) => d.etiqueta),
+          datasets: [
+              {
+                  data: this.datos_graficas.map((d) => d.valor),
+                  //backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+                  backgroundColor: this.datos_graficas.map((d) => documentStyle.getPropertyValue(`--${d.color}-500`)),                  
+                  //hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+                  hoverBackgroundColor: this.datos_graficas.map((d) => documentStyle.getPropertyValue(`--${d.color}-400`)),
+              }
+          ]
+      };
+
+      this.options = {
+          plugins: {
+              legend: {
+                  labels: {
+                      usePointStyle: true,
+                      color: textColor
+                  }
+              }
+          }
+      };
+  }
 }
---
+++ frontend/src/app/interfaces/datos_graficas.ts
@@ -0,0 +1,5 @@
+export interface datos_graficas {
+    etiqueta: string;
+    color?: string;
+    valor: number;
+}
