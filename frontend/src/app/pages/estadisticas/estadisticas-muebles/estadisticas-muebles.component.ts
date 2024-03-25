9b95147a5876b67bc74f362188cd6bd3a98af82e
Christian <christian.berdejo@tdconsulting.es>
2024-03-25 13:29:39 +0100

    small progress

--- frontend/src/app/pages/estadisticas/estadisticas-muebles/estadisticas-muebles.component.ts
+++ frontend/src/app/pages/estadisticas/estadisticas-muebles/estadisticas-muebles.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { resultados_ordenados_elementos } from 'src/app/interfaces/resultados_ordenados';
+import { ElementosService } from 'src/app/servicios/elementos/elementos.service';
 
 @Component({
   selector: 'app-estadisticas-muebles',
   templateUrl: './estadisticas-muebles.component.html',
   styleUrls: ['./estadisticas-muebles.component.css']
 })
-export class EstadisticasMueblesComponent {
- constructor  ( 
- ){}
+export class EstadisticasMueblesComponent implements OnInit  {
+ constructor  ( private elementosService: ElementosService ){ }
+
+ estadisticas: resultados_ordenados_elementos;
+ ngOnInit(): void {  
+  this.obtenerEstadisticas();
+ }
+
+ obtenerEstadisticas() {
+  this.elementosService.getEstadisticasRespuestas().subscribe(
+    (estadisticas: resultados_ordenados_elementos) => {
+      this.estadisticas = estadisticas;
+    });
+ }
+
 }
