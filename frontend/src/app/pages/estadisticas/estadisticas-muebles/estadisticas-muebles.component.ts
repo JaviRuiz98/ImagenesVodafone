f1570f92aba42662be16e0c9942e8b9c81714f6b
Christian <christian.berdejo@tdconsulting.es>
2024-03-26 11:07:17 +0100

    estadisticaMethods

--- frontend/src/app/pages/estadisticas/estadisticas-muebles/estadisticas-muebles.component.ts
+++ frontend/src/app/pages/estadisticas/estadisticas-muebles/estadisticas-muebles.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { datos_graficas } from 'src/app/interfaces/datos_graficas';
 import { resultados_ordenados_elementos } from 'src/app/interfaces/resultados_ordenados';
 import { ElementosService } from 'src/app/servicios/elementos/elementos.service';
+import { EstadisticasMethodsService } from 'src/app/shared/estadisticas/estadisticas-methods.service';
 
 @Component({
   selector: 'app-estadisticas-muebles',
@@ -8,17 +10,18 @@ import { ElementosService } from 'src/app/servicios/elementos/elementos.service'
   styleUrls: ['./estadisticas-muebles.component.css']
 })
 export class EstadisticasMueblesComponent implements OnInit  {
- constructor  ( private elementosService: ElementosService ){ }
+ constructor  ( private elementosService: ElementosService, private   estadisticasMethos: EstadisticasMethodsService ){ }
 
- estadisticas: resultados_ordenados_elementos;
+ estadisticas_resultados_carteles: datos_graficas[] = [];
+ estadisticas_resultados_conteo: datos_graficas[] = [];
  ngOnInit(): void {  
   this.obtenerEstadisticas();
  }
 
  obtenerEstadisticas() {
+
   this.elementosService.getEstadisticasRespuestas().subscribe(
     (estadisticas: resultados_ordenados_elementos) => {
-      this.estadisticas = estadisticas;
     });
  }
 
